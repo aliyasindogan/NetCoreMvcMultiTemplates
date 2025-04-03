e938944986cc465b81bf5afc1228161bde22f4cc
aliyasindogan <a.yasindogan@gmail.com>
2025-04-04 01:36:36 +0300

    ViewComponent ile navbar ve footer template yapıldı.

---
+++ NetCoreMvcMultiTemplates/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMvcMultiTemplates.ViewComponents
+{
+    public class FooterViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return await Task.FromResult<IViewComponentResult>(View("Footer1"));
+        }
+    }
+}
---
+++ NetCoreMvcMultiTemplates/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreMvcMultiTemplates.ViewComponents
+{
+    public class NavbarViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return await Task.FromResult<IViewComponentResult>(View("Navbar3"));
+        }
+    }
+}
